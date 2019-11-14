cbdbd88a2f74d87298433b38d199e5017a276a23
Beata von Grothusen <beatavg@n168-p37.eduroam.kth.se>
2019-11-14 12:10:18 +0100

    click on present working

---
+++ Assets/Scripts/OpenPresent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenPresent : MonoBehaviour
+{    
+    public GameObject present;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //transform.GetChild(2).gameObject.SetActive(true);
+            //gameObject.SetActive(false);
+            Vector3 p = transform.position;
+            Destroy(gameObject);
+            Instantiate(present, p, Quaternion.identity);
+
+
+        }
+
+    }
+}
