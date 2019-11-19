887a069d23a0c0e04eb5262cb08485d1a3c2a23e
Beata von Grothusen <beatavg@Beatas-MacBook-Air.local>
2019-11-19 20:15:35 +0100

    remove warning

--- Assets/Scripts/OpenPresent.cs
+++ Assets/Scripts/OpenPresent.cs
@@ -4,16 +4,11 @@ using System.Collections;
 public class OpenPresent : MonoBehaviour
 {   
     public GameObject contains;
-    private GameObject clone = null;
 
     void OnMouseDown(){
         Vector3 p = transform.position;
         this.gameObject.SetActive(false);
         contains.SetActive(true);
-  }
-
-  void Update() {
-    
   }
 
 }
