c532124f109aa455650d4b0ab5ae047afef987a0
Beata von Grothusen <beatavg@n132-p182.eduroam.kth.se>
2019-11-20 16:13:28 +0100

    throw workinggit add .

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -13,4 +13,15 @@ public class PickUp : MonoBehaviour
 
         transform.position = objPosition;
     }
+    void OnMouseUp() {
+        ThrowBall();
+    }
+
+    void ThrowBall()
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().AddForce(0f, 0f, 10000f); 
+    }
+
+
 }
