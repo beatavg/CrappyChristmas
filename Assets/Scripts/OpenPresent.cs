61bbbe48d739d70f2167f23d9927b4005a885346
Gabriella Westman <gabriella@n145-p147.eduroam.kth.se>
2019-11-29 11:56:37 +0100

    sound presents working

--- Assets/Scripts/OpenPresent.cs
+++ Assets/Scripts/OpenPresent.cs
@@ -4,19 +4,19 @@ using System.Collections;
 public class OpenPresent : MonoBehaviour
 {   
     public GameObject contains;
-    public AudioSource Source;
+    //public AudioSource Source;
 
-    void Awake(){
-    	Source = GetComponent<AudioSource>();
-    }
+    //void Awake(){
+    	//Source = GetComponent<AudioSource>();
+    //}
 
     void OnMouseDown(){
         Vector3 p = transform.position;
         this.gameObject.SetActive(false);
         contains.SetActive(true);
 
-        Source.Play();
-        Debug.Log("funkar");
+        //Source.Play();
+        //Debug.Log("funkar");
   }
 
 }
