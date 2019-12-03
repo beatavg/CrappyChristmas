67dcbece601fbf0b51a7236dd27e4c9b935c3678
Beata von Grothusen <beatavg@n144-p32.eduroam.kth.se>
2019-12-03 15:05:43 +0100

    added canvas window

---
+++ Assets/Scripts/CloseWondow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseWondow : MonoBehaviour
+{
+    public Button button;
+    public GameObject window = GameObject.Find("/Canvas/Image");
+    void Start()
+    {
+        button.onClick.AddListener(Close);
+    }
+
+    void Close()
+    {
+        window.SetActive(false);
+    }
+}
