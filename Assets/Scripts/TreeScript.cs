d6cf936c02057d909f2532ecbb013362e97e5f3a
Hedvig <hedvigfahlstedt@gmail.com>
2019-12-11 19:58:22 +0100

    Two different light solutions in one
    Uses both directional light and point light. Disable directional light in tree_script and enable plane_floor, disable plane_transparent and you get the more realistic shadow solution but with a visable floor. Keep the settings as they are and you get see trough floor with shadows from a directional light, not super realistic, gives you shadows in AR

--- Assets/Scripts/TreeScript.cs
+++ Assets/Scripts/TreeScript.cs
@@ -8,15 +8,20 @@ public class TreeScript : MonoBehaviour
 
     public GameObject Sphere;
     public GameObject WhiteSmoke;
+    public GameObject Sphere_directional;
 
     private void OnTriggerEnter(Collider col)
     {
         WhiteSmoke.SetActive(true);
-        Debug.Log("WhiteSmoke!");
+        //Debug.Log("WhiteSmoke!");
         var lt = Sphere.GetComponent<Light>();
         lt.intensity = lt.intensity + 5F;
-        WhiteSmoke.SetActive(true);
-        Debug.Log("Light intensifies!");
+
+        var lt2 = Sphere_directional.GetComponent<Light>();
+        lt2.intensity = 4F;
+        
+            WhiteSmoke.SetActive(true);
+        //Debug.Log("Light intensifies!");
 
         if (col.tag == "Shoe")
         {
