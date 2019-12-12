c3aade1a4853df6653fd71b6c520fe01cf31ef4c
Hedvig <hedvigfahlstedt@gmail.com>
2019-12-12 21:55:25 +0100

    Created TreeScript2
    Changed treescript back to original before merge with master

--- Assets/Scripts/TreeScript.cs
+++ Assets/Scripts/TreeScript.cs
@@ -5,24 +5,8 @@ using UnityEngine;
 
 public class TreeScript : MonoBehaviour
 {
-
-    public GameObject Sphere;
-    public GameObject WhiteSmoke;
-    public GameObject Sphere_directional;
-
     private void OnTriggerEnter(Collider col)
     {
-        WhiteSmoke.SetActive(true);
-        //Debug.Log("WhiteSmoke!");
-        var lt = Sphere.GetComponent<Light>();
-        lt.intensity = lt.intensity + 5F;
-
-        var lt2 = Sphere_directional.GetComponent<Light>();
-        lt2.intensity = 4F;
-        
-            WhiteSmoke.SetActive(true);
-        //Debug.Log("Light intensifies!");
-
         if (col.tag == "Shoe")
         {
             ProgressBar.CO2 += 100;
---
+++ Assets/Scripts/TreeScript2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TreeScript2 : MonoBehaviour
+{
+
+    public GameObject Sphere;
+    public GameObject WhiteSmoke;
+    public GameObject Sphere_directional;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        WhiteSmoke.SetActive(true);
+        //Debug.Log("WhiteSmoke!");
+        var lt = Sphere.GetComponent<Light>();
+        lt.intensity = lt.intensity + 2F;
+
+        var lt2 = Sphere_directional.GetComponent<Light>();
+        lt2.intensity = 4F;
+
+        WhiteSmoke.SetActive(true);
+        //Debug.Log("Light intensifies!");
+
+        if (col.tag == "Shoe")
+        {
+            ProgressBar.CO2 += 100;
+        }
+        else if (col.tag == "Ball")
+        {
+            ProgressBar.CO2 += 60;
+        }
+        else if (col.tag == "Toy")
+        {
+            ProgressBar.CO2 += 50;
+        }
+
+    }
+}
